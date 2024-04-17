dc3c4dfef8e48de28ac587dea4c35c17f6ed56b6
Marxu <fuhaojun@52tt.com>
2024-04-17 15:02:10 +0800

    feat: 写一个 模拟电商页面的 graphql  schema

--- server.js
+++ server.js
@@ -4,14 +4,63 @@ const { graphqlHTTP } = require('express-graphql')
 
 const schema = buildSchema(`
     type Query{
-        description: String
-        price: Float
+        products: [Product]
+        orders: [Order]
+    }
+
+    type Product{
+        id: ID!
+        description: String!
+        reviews: [Review]
+        price: Float!
+    }
+
+    type Review{
+        rating: Int! 
+        comment: String
+    }
+
+    type Order{
+        date: String!
+        subtotal: Float!
+        items: [OrderItem]
+    }
+
+    type OrderItem{
+        product: Product!
+        quantity: Int!
     }
 `)
 
 const root = {
-    description: 'Cool clothes',
-    price: 99.99
+    products: [
+        {
+        id: 'redshoes',
+        description: 'Red shoes',
+        price: 12.12
+        },
+        {
+            id: 'yellowshoes',
+            description: 'Yellow shoes',
+            price: 12.12
+            }
+    ],
+    orders: [
+        {
+            date:'2024-4-1',
+            subtotal: 90.12,
+            item:[
+               {
+                 product:{
+                    id:'redshoes',
+                    description: 'Old one',
+                    price: 45.22
+                },
+                quantity: 2
+            }
+            ]
+        }
+    ]
 }
 
 const app = express()
